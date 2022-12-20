ba53f055bb1b7b3045fe2df0e11b2120fa6f968b
SamiA86 <samueljunk86@gmail.com>
2022-12-20 02:54:14 -0800

    python classes

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,24 +1,12 @@
 #!/usr/bin/python3
-"""Define classes for a singly-linked list."""
-
-
 class Node:
-    """Represent a node in a singly-linked list."""
-
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
@@ -28,8 +16,7 @@ class Node:
 
     @property
     def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
@@ -39,14 +26,32 @@ class Node:
 
 
 class SinglyLinkedList:
-    """Represent a singly-linked list."""
+    def __str__(self):
+        rtn = ""
+        ptr = self.__head
+
+        while ptr is not None:
+            rtn += str(ptr.data)
+            if ptr.next_node is not None:
+                rtn += "\n"
+            ptr = ptr.next_node
+
+        return rtn
 
     def __init__(self):
-        """Initalize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
-        """Insert a new Node to the SinglyLinkedList.
+        ptr = self.__head
 
-        The node is inserted into the list at the correct
+        while ptr is not None:
+            if ptr.data > value:
+                break
+            ptr_prev = ptr
+            ptr = ptr.next_node
 
+        newNode = Node(value, ptr)
+        if ptr == self.__head:
+            self.__head = newNode
+        else:
+            ptr_prev.next_node = newNode
